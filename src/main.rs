d7d0c266cc5270b42737aac680f177f891bf39a2
Alexi Peck <alexinicolaspeck@gmail.com>
2022-11-05 02:32:49 +1100

    .

--- src/main.rs
+++ src/main.rs
@@ -120,93 +120,55 @@ fn day_2_part_2(input_path: &Path) {
     );
 }
 
-#[allow(dead_code)]
-struct BitTracker {
-    pub bits_of_bits: Vec<HashMap<char, u32>>,
-}
-
-impl BitTracker {
-    #[allow(dead_code)]
-    pub fn new(bit_count: usize) -> Self {
-        Self {
-            bits_of_bits: vec![HashMap::new(); bit_count]
-        }
-    }
-}
-
-impl BitTracker {
-    #[allow(dead_code)]
-    fn increment(&mut self, bit: &char, position: usize) {
-        if !self.bits_of_bits[position].contains_key(bit) {
-            self.bits_of_bits[position].insert(*bit, 0);
-        }
-        *self.bits_of_bits[position].get_mut(bit).unwrap() += 1;
-    }
-
-    #[allow(dead_code)]
-    fn gamma_binary(&self) -> String {
-        self.bits_of_bits.iter().map(|k| {
-            let mut chosen_bit: Option<char> = None;
-            let mut highest_count: u32 = 0;
-            for (bit, count) in k {
-                if count > &highest_count {
-                    chosen_bit = Some(*bit);
-                    highest_count = *count;
-                }
-            }
-            chosen_bit.unwrap()
-        }).collect::<String>()
-    }
-
-    #[allow(dead_code)]
-    fn epsilon_binary(&self) -> String {
-        self.bits_of_bits.iter().map(|k| {
-            let mut chosen_bit: Option<char> = None;
-            let mut lowest_count: u32 = u32::MAX;
-            for (bit, count) in k {
-                if count < &lowest_count {
-                    chosen_bit = Some(*bit);
-                    lowest_count = *count;
-                }
-            }
-            chosen_bit.unwrap()
-        }).collect::<String>()
-    }
-}
 
 #[allow(dead_code)]
 fn day_3_part_1(input_path: &Path) {
-    let mut bit_counter = BitTracker::new(12);
+    let mut bits_of_bits: Vec<HashMap<char, u32>> = vec![HashMap::new(); 12];
     for bits_x12 in get_lines(input_path) {
         for (position, bit) in bits_x12.chars().into_iter().enumerate() {
-            bit_counter.increment(&bit, position);
+            bits_of_bits[position].entry(bit).or_insert(0);
+            *bits_of_bits[position].get_mut(&bit).unwrap() += 1;
         }
     }
-
-    let gamma_bits = bit_counter.gamma_binary();
-    let gamma_int = usize::from_str_radix(&gamma_bits, 2).unwrap();
-    println!("{}: {}", gamma_bits, gamma_int);
-
-    let epsilon_bits = bit_counter.epsilon_binary();
-    let epsilon_int = usize::from_str_radix(&epsilon_bits, 2).unwrap();
-    println!("{}: {}", epsilon_bits, epsilon_int);
-
-    let gamma_x_epsilon = gamma_int * epsilon_int;
-    println!("{}", gamma_x_epsilon);
+    let gamma_bits = bits_of_bits.iter().map(|t| {
+        let mut chosen_bit: Option<char> = None;
+        let mut highest_count: u32 = 0;
+        for (bit, count) in t {
+            if count > &highest_count {
+                chosen_bit = Some(*bit);
+                highest_count = *count;
+            }
+        }
+        chosen_bit.unwrap()
+    }).collect::<String>();
+    let epsilon_bits = gamma_bits.chars().into_iter().map(|bit| if bit == '0' {'1'} else {'0'}).collect::<String>();
+    println!("{}", usize::from_str_radix(&gamma_bits, 2).unwrap() * usize::from_str_radix(&epsilon_bits, 2).unwrap());
 }
 
 #[allow(dead_code)]
 fn day_3_part_2(input_path: &Path) {
+    let mut bits_of_bits: Vec<HashMap<char, u32>> = vec![HashMap::new(); 12];
     for bits_x12 in get_lines(input_path) {
         for (position, bit) in bits_x12.chars().into_iter().enumerate() {
-            println!("{}: {}", position, bit);
+            bits_of_bits[position].entry(bit).or_insert(0);
+            *bits_of_bits[position].get_mut(&bit).unwrap() += 1;
         }
     }
+    let mut most_common_bits = [None; 12];
+    for (i, t) in bits_of_bits.iter().enumerate() {
+        let mut chosen_bit: char = '1';
+        if t.get(&'0').unwrap() > t.get(&'1').unwrap() {
+            chosen_bit = '0';
+        }
+        most_common_bits[i] = Some(chosen_bit);
+    }
+    let most_common_bits = most_common_bits.iter().map(|t| t.unwrap()).collect::<String>();
+    let least_common_bits = most_common_bits.chars().into_iter().map(|bit| if bit == '0' {'1'} else {'0'}).collect::<String>();
+    println!("{}", most_common_bits);
 }
 
 fn main() {
     let current_directory = env::current_dir().unwrap();
-    println!("{}", current_directory.display());
     //day_1_part_1(&current_directory.join("input_day_1.txt"));
     //day_1_part_2(&current_directory.join("input_day_1.txt"));
 
